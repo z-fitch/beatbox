62b2ea91d774a424b942e7d629c4602765e5daf9
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-12 15:42:54 -0500

    add node tests

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Node do
         
         
         expect(node).to be_instance_of(Node)
-        
+        expect(node.data).to eq("plop")
+        expect(node.next_node).to eq(nil)
     end
 
 end
