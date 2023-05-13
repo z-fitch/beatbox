ccc36a2e6c50d17d34844a290fee482de1130b17
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-13 16:13:45 -0500

    add append tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,14 +2,29 @@ require "./lib/linked_list"
 require "./lib/node"
 
 RSpec.describe LinkedList do
-    it "exists" do
-        node = Node.new("plop")
-        list = LinkedList.new
-        
-        expect(list).to be_instance_of(LinkedList)
-        expect(list.head).to eq(nil)
-        
+    before( :each) do
+        @node = Node.new("plop")
+        @list = LinkedList.new
     end
+
+    describe '#initialize' do
+        it "exists" do
+            
+            
+            expect(@list).to be_instance_of(LinkedList)
+            expect(@list.head).to eq(nil)
+            expect(@list.head).to eq(nil) 
+            
+        end
+    end
+    
+    describe '#append' do
+        it "exists" do
+            expect(@list.append("doop")).to eq("doop")
+            expect(@list.head.next_node).to eq(nil) 
+        end
+    end
+
 
 
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,13 +3,14 @@ require "./lib/node"
 
 
 RSpec.describe Node do
-    it "exists" do
-        node = Node.new("plop")
-        
-        
-        expect(node).to be_instance_of(Node)
-        expect(node.data).to eq("plop")
-        expect(node.next_node).to eq(nil)
+    describe "#initialize(data)" do 
+        it "exists" do
+            node = Node.new("plop")
+            
+            
+            expect(node).to be_instance_of(Node)
+            expect(node.data).to eq("plop")
+            expect(node.next_node).to eq(nil)
+        end
     end
-
 end
