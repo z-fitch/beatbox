5f70d53dd6c3fc615026bcbfdffb0064ae14450e
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-15 18:02:19 -0500

    feat: add insert method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -21,28 +21,6 @@ def append(data)
     end
 end
 
-def count
-    counter = 0
-    current_node = @head
-    until current_node == nil
-    current_node = current_node.next_node
-    counter += 1
-    end
-    counter
-end
-
-
-
-def to_string
-    current_node = @head
-    bb = []
-        until current_node == nil
-            bb << current_node.data
-            current_node = current_node.next_node
-        end
-    bb.join(" ")
-end
-
 def prepend(data)
     current_node = Node.new(data)
     current_node.next_node = @head
@@ -50,11 +28,38 @@ def prepend(data)
 end
 
 
+# 
 
-def insert
-
+def count
+    counter = 0
+    current_node = @head
+    until current_node == nil
+        current_node = current_node.next_node
+        counter += 1
+    end
+    counter
 end
 
+
+def to_string
+    current_node = @head
+    bb = []
+    until current_node == nil
+        bb << current_node.data
+        current_node = current_node.next_node
+    end
+    bb.join(" ")
+end
+
+def insert(position, data)
+    current_node = @head
+    (position - 1).times do
+        current_node = current_node.next_node
+    end
+    new_node = Node.new(data)
+    new_node.next_node = current_node.next_node 
+    current_node.next_node = new_node 
+end
 
 end
 
